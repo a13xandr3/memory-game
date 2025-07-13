a0f8d35032351c640799b1ca5d96394ea6136564
Alexandre Esteves <a13xandr3ea@gmail.com>
2025-07-13 02:38:34 -0300

    feat: Ajuste funcionalidade ordenação e layout imagens

--- src/app/corpo/corpo.component.ts
+++ src/app/corpo/corpo.component.ts
@@ -2,6 +2,7 @@ import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { ScreenService } from '../../core/service/screen.service';
 import { NivelJson } from '../../core/model/nivel.interface';
 import { HttpErrorResponse } from '@angular/common/http';
+import { catchError, concatMap, map, Observable, of, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-corpo',
@@ -9,43 +10,68 @@ import { HttpErrorResponse } from '@angular/common/http';
   styleUrls: ['./corpo.component.scss']
 })
 export class CorpoComponent implements OnInit, AfterViewInit {
+  nivelSelecionado = 'Facil';
+
+  imagensCertas = [];
+  modoFacil!: [];
+  modoDificil!: [];
+
   urlImage = '../../assets/img/';
-  modoFacil!: string[];
-  modoDificil!: string[];
 
   constructor(
     private screenService: ScreenService,
-  ) {}
+  ) {
+  }
 
   ngOnInit(): void {
     let corpo = this.corpo;
     if ( corpo ) {
       corpo.style.height = Number(localStorage.getItem('bodyVH')) + 'vh';
     }
-    this.getNiveis();
+    this.executarEmOrdem();
   }
 
-  ngAfterViewInit(): void {}
+  ngAfterViewInit(): void {
+  }
 
   get corpo(): HTMLElement {
     return document.getElementById('corpo') as HTMLElement;
   }
 
-  getNiveis(): void {
-    this.screenService.getNiveis().pipe().subscribe({
-      next: (dados: NivelJson) => {
-        
-        this.modoFacil = dados.Nivel.facil;
-        this.modoDificil = dados.Nivel.dificil;
-
-        //console.log('Fácil:', dados.Nivel.facil);
-        //console.log('Difícil:', dados.Nivel.dificil);
-      },
-      error: (err: HttpErrorResponse) => {
-        console.error('Status:', err.status);        // Ex: 404, 500
-        console.error('Mensagem:', err.message);     // Mensagem descritiva
-        console.error('Erro completo:', err);        // Objeto detalhado
-      }
-    })
+  getNiveis(): Observable<any> {
+    return this.screenService.getNiveis().pipe(
+      map((dados: NivelJson) => {
+        return dados.Nivel.facil;  // devolve o array direto
+      }),
+      catchError((err: HttpErrorResponse) => {
+        console.error('Status:', err.status);
+        console.error('Mensagem:', err.message);
+        console.error('Erro completo:', err);
+        return throwError(() => err);  // reemite erro pro fluxo
+      })
+    );
   }
+
+  executarEmOrdem(): any {
+    this.getNiveis().pipe(
+      concatMap((resultadoPrimeira: any) => {
+        return this.shuffle(resultadoPrimeira);
+      })
+    ).subscribe((resultadoSegunda: any) => {
+      this.modoFacil = resultadoSegunda;
+      console.log('Array embaralhado:', resultadoSegunda);
+    });
+  }
+
+///
+
+  // Função para embaralhar um array (algoritmo Fisher-Yates)
+  shuffle(array: any) {
+      for (let i = array?.length - 1; i > 0; i--) {
+          let j = Math.floor(Math.random() * (i + 1));
+          [array[i], array[j]] = [array[j], array[i]]; // Troca os elementos
+      }
+    return of(array).pipe();
+  }
+
 }
